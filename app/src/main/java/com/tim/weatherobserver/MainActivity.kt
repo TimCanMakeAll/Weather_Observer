4128e867c05342a6d9df672a099704d83038a377
Tim1 <timofeiverbickii4@gmail.com>
2023-01-04 22:59:34 +0300

    Initial commit

---
+++ app/src/main/java/com/tim/weatherobserver/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.tim.weatherobserver
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.tim.weatherobserver.Fragments.DetailsFragment
+import com.tim.weatherobserver.Fragments.HomeFragment
+import com.tim.weatherobserver.Fragments.SettingsFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var homeFragment = HomeFragment()
+        val settingsFragment = SettingsFragment()
+        val detailsFragment = DetailsFragment()
+
+        makeCurrentFragment(settingsFragment)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigation.selectedItemId = R.id.ic_settings
+
+        bottomNavigation.setOnItemSelectedListener{
+            when(it.itemId){
+
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_settings -> makeCurrentFragment(settingsFragment)
+                R.id.ic_detail -> makeCurrentFragment(detailsFragment)
+            }
+            true
+        }
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment){
+
+        supportFragmentManager.beginTransaction().apply {
+
+            replace(R.id.fl_wrapper, fragment)
+
+            commit()
+        }
+    }
+}
