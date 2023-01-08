3038085201a56e0520f49600de6a64ffeb2a8c2b
Tim1 <timofeiverbickii4@gmail.com>
2023-01-08 21:16:27 +0300

    created layouts for home screen and detail info fragments, changed status bar and bottomNavigation colors by fragments themes

--- app/src/main/java/com/tim/weatherobserver/MainActivity.kt
+++ app/src/main/java/com/tim/weatherobserver/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.tim.weatherobserver
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.tim.weatherobserver.Fragments.DetailsFragment
@@ -9,6 +16,8 @@ import com.tim.weatherobserver.Fragments.HomeFragment
 import com.tim.weatherobserver.Fragments.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,18 +32,33 @@ class MainActivity : AppCompatActivity() {
 
         bottomNavigation.selectedItemId = R.id.ic_settings
 
-        bottomNavigation.setOnItemSelectedListener{
-            when(it.itemId){
+        bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
 
-                R.id.ic_settings -> makeCurrentFragment(settingsFragment)
-                R.id.ic_home -> makeCurrentFragment(homeFragment)
-                R.id.ic_detail -> makeCurrentFragment(detailsFragment)
+                R.id.ic_settings -> {
+                    makeCurrentFragment(settingsFragment)
+                    val window = this.window
+                    window.statusBarColor = this.resources.getColor(R.color.teal_800)
+                    bottomNavigation.setBackgroundResource(R.color.teal_800)
+                }
+                R.id.ic_home -> {
+                    makeCurrentFragment(homeFragment)
+                    val window = this.window
+                    window.statusBarColor = this.resources.getColor(R.color.blue_dark)
+                    bottomNavigation.setBackgroundResource(R.color.blue_dark)
+                }
+                R.id.ic_detail -> {
+                    makeCurrentFragment(detailsFragment)
+                    val window = this.window
+                    window.statusBarColor = this.resources.getColor(R.color.red_dark)
+                    bottomNavigation.setBackgroundResource(R.color.red_dark)
+                }
             }
             true
         }
     }
 
-    private fun makeCurrentFragment(fragment: Fragment){
+    private fun makeCurrentFragment(fragment: Fragment) {
 
         supportFragmentManager.beginTransaction().apply {
 
